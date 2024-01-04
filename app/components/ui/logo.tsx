506390789e56cb6afe511720407975ef768e0699
chenqiaoda <1306046613@qq.com>
2024-01-04 16:32:03 +0800

    fix: 文字修改

--- app/components/ui/logo.tsx
+++ app/components/ui/logo.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 export default function Logo() {
     return (
         <Link href="/" className="flex justify-center items-center" aria-label="Cruip">
-            <Image src="/image/logo.png" width={81} height={76} alt='Risk control' />
-            <span className=' text-xs text-white'>Risk control</span>
+            <Image src="/image/logo.png" width={81} height={76} alt='AI Risk control' />
+            <span className=' text-xs text-white'>AI Risk control</span>
         </Link>
     )
 }

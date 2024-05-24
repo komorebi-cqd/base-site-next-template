4e8f3b0af90d12f3761dd043ab8223df0732f320
chenqiaoda <1306046613@qq.com>
2024-05-24 10:08:02 +0800

    fix: 改链接

--- app/components/Btn/JumpBtn.tsx
+++ app/components/Btn/JumpBtn.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const JumpBtn = ({ locale, children }: { locale: string,children: React.ReactNode }) => {
     return (
-        <a href={locale === 'en' ? process.env.NEXT_PUBLIC_JUMP_URL_EN : process.env.NEXT_PUBLIC_JUMP_URL_ZH} target="_blank" className="w-[206px] h-[56px] bg-white rounded leading-[56px] text-primary text-center text-xl font-semibold">
+        <a href={locale === 'en' ? process.env.NEXT_PUBLIC_JUMP_URL_EN : process.env.NEXT_PUBLIC_JUMP_URL_ZH} target="_blank" className=" cursor-pointer w-[206px] h-[56px] bg-white rounded leading-[56px] text-primary text-center text-xl font-semibold">
             {children}
         </a>
     )
